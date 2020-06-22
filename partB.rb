bd80a59bf62134485cc2dea4d551414c145d8d0b
Stephen <stephen_h369@live.co.uk>
2020-06-22 14:37:06 +0100

    get team name passes

--- partB.rb
+++ partB.rb
@@ -6,4 +6,15 @@ class Team
         @coach = coach
     end
 
+    def name()
+        return @name
+    end
+
+    def players()
+        return @players
+    end
+
+    def coach()
+        return @coach
+    end
 end
--- specs/partB_spec.rb
+++ specs/partB_spec.rb
@@ -5,5 +5,9 @@ require_relative('../partB.rb')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new()
 
 class TestClasses < Minitest::Test
-    
+
+    def test_get_team_name()
+        new_team = Team.new("The not very good team", ["Stephen", "Bob", "John"], "Your dad")
+        assert_equal("The not very good team", new_team.name())
+    end
 end
